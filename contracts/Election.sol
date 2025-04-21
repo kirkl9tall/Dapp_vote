a1df6ede6471c7691e87a89422daac04a2c3d743
the_worst <kirkassassin@gmail.com>
2025-04-21 23:05:10 +0100

    deploy first contract

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.29;
+
+contract Election{
+    // store  candidate
+    // read  candidate 
+    string  public candidate;
+    //Constructor 
+    constructor (){
+        candidate =  "Candidate 1";
+    }
+}
--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,20 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
+  compilers: {
+    solc: {
+      version: "0.8.29", // Must match your pragma
+      settings: {
+        optimizer: {
+          enabled: true, // Optional: Reduces gas costs
+          runs: 200,
+        },
+      },
+    },
+  },
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      port: 8545, // Default Ganache port
+      network_id: "*",
     },
-    develop: {
-      port: 8545
-    }
-  }
+  },
 };
